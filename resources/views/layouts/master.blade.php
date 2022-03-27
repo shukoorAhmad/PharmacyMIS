7f482cd575d1072a31312fde42e29a7d396bc014
Subhan Waisy <92630562+MahboobSubhanWaisy@users.noreply.github.com>
2022-03-27 15:03:17 +0430

    Space Removed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -222,7 +222,6 @@
                             </ul>
                         </div>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link" href="{{route('stock')}}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar link-icon">
