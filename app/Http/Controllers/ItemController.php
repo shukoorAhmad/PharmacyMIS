528f97029cfe10ba95ffee1ae86d9c366399aa47
shukoorAhmad <45222764+shukoorAhmad@users.noreply.github.com>
2022-03-27 11:15:34 +0430

    Update ItemController.php

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
+use App\Models\Measure_unit;
 use App\Models\Supplier;
 use Illuminate\Http\Request;
 
@@ -13,26 +15,30 @@ class ItemController extends Controller
         $data['supplier'] = Supplier::all();
         return view('item.create', $data);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function showItemField()
     {
-        //
+        $measure = Measure_unit::all();
+        $data = "<div class='col-md-12 show_items' style='padding:0 !important;'><div class='row'>";
+        $data .= "<div class='col-md-4'><label>Item Name</label><input name='item_name[]' class='form-control' required></div>";
+        $data .= "<div class='col-md-3'><label>Measure</label><select class='form-control' name='measure_id[]' required><option selected disabled>Select Measure Unit</option>";
+        foreach ($measure as $m) {
+            $data .= "<option value='" . $m->measure_unit_id . "'>" . $m->unit . "</option>";
+        }
+        $data .= "</select></div>";
+        $data .= "<div class='col-md-3'><label>Quantity Per Carton</label><input name='qty_per_carton[]' required class='form-control'></div>";
+        $data .= "<div class='col-md-2'><a class='btn btn-danger w-100 close_btn' style='padding: 7px 1.75rem !important;margin-left:-12px !important;margin-top:30px !important;font-size:14px !important;'><i class='zmdi zmdi-close text-white'></i></a></div>";
+        $data .= "</div></div>";
+        return $data;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
-        //
+        // dd($request->all());
+        $counter = $request->item_name;
+        foreach ($counter as $count) {
+            dd($count);
+        }
     }
 
     /**
@@ -65,17 +71,6 @@ class ItemController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
     {
         //
     }
