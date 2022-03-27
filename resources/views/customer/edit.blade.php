be6b0458eec6c2217d0973ffa8a24623e60835d5
shukoorAhmad <45222764+shukoorAhmad@users.noreply.github.com>
2022-03-27 14:57:46 +0430

    customer updated

--- resources/views/customer/edit.blade.php
+++ resources/views/customer/edit.blade.php
@@ -21,41 +21,41 @@
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header bg-primary">
-                <h3 class="modal-title text-white">Edit Item {{ $item->item_name }}</h3>
+                <h4 class="modal-title text-white">Edit Customer {{$customer->pharmacy_name}} </h4>
             </div>
-            <form action="{{ route('itemupdate') }}" method="post">
+            <form action="{{ route('update-customer') }}" method="post">
                 @csrf
                 <div class="modal-body">
                     <div class="form-group">
-                        <label class="col-form-label">Item Name</label>
-                        <input class="form-control" name="item_name" value="{{ $item->item_name }}" autofocus required>
-                        <input type="hidden" name="id" value="{{ $item->item_id }}">
+                        <label class="col-form-label">Pharmacy Name</label>
+                        <input class="form-control" name="pharmacy_name" value="{{ $customer->pharmacy_name }}" autofocus required>
+                        <input type="hidden" name="id" value="{{ $customer->customer_id }}">
                     </div>
                     <div class="form-group">
-                        <label class="col-form-label">Stock Address</label>
-                        <input class="form-control" name="qty_per_carton" value="{{ $item->quantity_per_carton }}">
+                        <label class="col-form-label">Customer Name</label>
+                        <input class="form-control" name="customer_name" value="{{ $customer->customer_name }}">
                     </div>
                     <div class="form-group">
-                        <label class="col-form-label">Supplier Name</label>
-                        <select name="supplier" class="form-control select2" style="width: 100% !important;">
-                            <option value="" selected disabled>Please Select Company</option>
-                            @foreach ($supplier as $sup)
-                            <option value="{{ $sup->supplier_id }}" {{ $sup->supplier_id == $item->supplier_id ? 'selected' : '' }}>
-                                {{ $sup->name }}
-                            </option>
+                        <label class="col-form-label">Customer Last Name</label>
+                        <input class="form-control" name="customer_last_name" value="{{ $customer->customer_last_name }}">
+                    </div>
+                    <div class="form-group">
+                        <label class="col-form-label">Customer Site</label>
+                        <select name="site_id" class="form-control select2" style="width: 100% !important;" required>
+                            @foreach($site as $site_row)
+                            <option value="{{$site_row->site_id}}" {{$site_row->site_id==$customer->site_id?'selected':''}}>{{$site_row->site_name.' '.$site_row->prov_id->en_province}}</option>
                             @endforeach
                         </select>
                     </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Supplier Name</label>
-                        <select name="measure_id" class="form-control select2" style="width: 100% !important;">
-                            <option value="" selected disabled>Please Select Company</option>
-                            @foreach ($measure_units as $unit)
-                            <option value="{{ $unit->measure_unit_id }}" {{ $unit->measure_unit_id == $item->measure_unit_id ? 'selected' : '' }}>
-                                {{ $unit->unit }}
-                            </option>
-                            @endforeach
-                        </select>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label class="col-form-label">Contact No 1</label>
+                            <input type="number" class="form-control" name="contact_no" value="{{$customer->contact_no}}" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxLength="10">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label class="col-form-label">Contact No 2</label>
+                            <input type="number" class="form-control" name="contact_no_2" value="{{$customer->contact_no_2}}" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" maxLength="10">
+                        </div>
                     </div>
                 </div>
                 <div class="modal-footer justify-content-between">
