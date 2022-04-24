153905170e3a7f2860971d61b40c8df5676da20f
Subhan Waisy <92630562+MahboobSubhanWaisy@users.noreply.github.com>
2022-04-24 11:27:33 +0430

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 // site crud
